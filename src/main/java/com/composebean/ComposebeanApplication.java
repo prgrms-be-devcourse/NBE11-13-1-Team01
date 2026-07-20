46560837e1d7f758ecdebd82d937eb9377e4af9d
dPdms21 <studye20@gmail.com>
2026-07-20 09:17:24 +0900

    Chore: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/composebean/ComposebeanApplication.java
@@ -0,0 +1,13 @@
+package com.composebean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComposebeanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComposebeanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/composebean/ComposebeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.composebean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComposebeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
