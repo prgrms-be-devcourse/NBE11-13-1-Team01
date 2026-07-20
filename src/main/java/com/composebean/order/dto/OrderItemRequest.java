9887d7a80d479f93c8c34a0403cd6f26187871fd
JuhyungKim-dev <jkim00781@gmail.com>
2026-07-20 17:09:50 +0900

    Feat: 주문 생성 및 결제 처리 기능 구현
    주문 요청과 생성 결과 전달을 위한 DTO 및 Repository를 구현했다.
    상품 가격을 기준으로 주문 금액을 계산하고 재고 차감과 결제 완료 상태 저장을 처리했다.

---
+++ src/main/java/com/composebean/order/dto/OrderItemRequest.java
@@ -0,0 +1,16 @@
+package com.composebean.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItemRequest {
+
+    private Long productId;
+    private Integer quantity;
+}
